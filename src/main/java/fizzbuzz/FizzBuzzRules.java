92a3deaf7233eb8206dd563680031106d9300281
Virjanand Panday <virjanand@virjanand.com>
2022-07-12 17:04:45 +0200

    Refactor: new rule: if 3 in number then return Fizz

--- src/main/java/fizzbuzz/FizzBuzzRules.java
+++ src/main/java/fizzbuzz/FizzBuzzRules.java
@@ -9,10 +9,11 @@ public class FizzBuzzRules {
     }
 
     private String getFizzRule(int number) {
-        if (Integer.toString(number).contains("3")) {
-            return "Fizz";
-        }
-        return isDivisibleBy(number, 3) ? "Fizz" : "";
+        return isDivisibleBy(number, 3) || numberContains(number, "3") ? "Fizz" : "";
+    }
+
+    private boolean numberContains(int number, String numberToBeContained) {
+        return Integer.toString(number).contains(numberToBeContained);
     }
 
     private String getBuzzRule(int number) {
