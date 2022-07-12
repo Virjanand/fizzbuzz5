f28424d88eaec2cae73017cdd8e587fdb18d5859
Virjanand Panday <virjanand@virjanand.com>
2022-07-12 16:52:21 +0200

    Instantiate in field

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -8,8 +8,10 @@ import java.util.stream.Stream;
 
 public class FizzBuzz {
 
+    private final FizzBuzzRules fizzBuzzRules = new FizzBuzzRules();
+
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, new FizzBuzzRules().getRules(number))).collect(Collectors.toList());
+        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, fizzBuzzRules.getRules(number))).collect(Collectors.toList());
     }
 
     private String toFizzBuzz(int number, Stream<Supplier<String>> rules) {
