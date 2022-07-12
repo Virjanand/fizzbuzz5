7d1a89fdd3032e9cad47949a02e4cce329c279a9
Virjanand Panday <virjanand@virjanand.com>
2022-07-12 16:58:09 +0200

    Extract rules in own methods

--- src/main/java/fizzbuzz/FizzBuzzRules.java
+++ src/main/java/fizzbuzz/FizzBuzzRules.java
@@ -5,7 +5,15 @@ import java.util.stream.Stream;
 
 public class FizzBuzzRules {
     public Stream<Supplier<String>> getRules(int number) {
-        return Stream.of(() -> convertNumberToFizzBuzz(number, 3, "Fizz"), () -> convertNumberToFizzBuzz(number, 5, "Buzz"));
+        return Stream.of(() -> getFizzRule(number), () -> getBuzzRule(number));
+    }
+
+    private String getFizzRule(int number) {
+        return convertNumberToFizzBuzz(number, 3, "Fizz");
+    }
+
+    private String getBuzzRule(int number) {
+        return convertNumberToFizzBuzz(number, 5, "Buzz");
     }
 
     private String convertNumberToFizzBuzz(int number, int divisor, String result) {
