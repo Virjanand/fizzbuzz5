8d06992827d237085f2b72f79ed683dbe815575c
Virjanand Panday <virjanand@virjanand.com>
2022-07-12 16:50:40 +0200

    Introduce class to contain rules to be injected

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,10 +6,23 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+class FizzBuzzRules {
+    public Stream<Supplier<String>> getRules(int number) {
+        return Stream.of(() -> convertNumberToFizzBuzz(number, 3, "Fizz"), () -> convertNumberToFizzBuzz(number, 5, "Buzz"));
+    }
+
+    private String convertNumberToFizzBuzz(int number, int divisor, String result) {
+        if (number % divisor == 0) {
+            return result;
+        }
+        return "";
+    }
+}
+
 public class FizzBuzz {
 
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, getRules(number))).collect(Collectors.toList());
+        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, new FizzBuzzRules().getRules(number))).collect(Collectors.toList());
     }
 
     private String toFizzBuzz(int number, Stream<Supplier<String>> rules) {
